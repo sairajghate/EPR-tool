05cc715cbb1135f63716e3fada869dc5aaaef472
sairajghate <73525307+sairajghate@users.noreply.github.com>
2026-02-08 16:33:21 +1030

    Update service worker cache version and cleanup

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
-const CACHE = "epr-plotter-v1";
+const CACHE = "epr-plotter-v2";
 const ASSETS = ["./", "./index.html", "./app.js", "./sw.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
